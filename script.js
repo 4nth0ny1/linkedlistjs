4d04689cf4dc81b8de2f74fec423478020d6a6f7
4nth0ny1 <acatullo4@gmail.com>
2022-01-07 23:13:44 +0530

    add linkedlist and node

---
+++ script.js
@@ -0,0 +1,11 @@
+function LinkedList(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}
+
+function Node() {
+    this.next = null;
+    this.prev = null;
+    this.content = null;
+}
